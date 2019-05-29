ec374fa50ad126de069e40a4fd510c6be49f3ee8
yathishgowdaa <36576748+yathishgowdaa@users.noreply.github.com>
2019-05-28 21:43:47 -0700

    added bootstarp and jobs

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
