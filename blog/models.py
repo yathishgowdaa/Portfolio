a4423c197938893f6f93a19cdae8e4ffa1d544fe
yathishgowdaa <36576748+yathishgowdaa@users.noreply.github.com>
2019-05-29 12:11:25 -0700

    blog changes and final polish

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,14 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
 
 
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %y')
+
+    def __str__(self):
+        return  self.title
+
+
+
